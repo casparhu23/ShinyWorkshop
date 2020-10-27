891fc20f554aa8bd0b84443496bb4829ba26281b
Joe <jjenkin7@u.rochester.edu>
2020-10-26 23:11:38 -0400

    Shiny Workshops initial commit

---
+++ ShinyMapExample.R
@@ -0,0 +1,74 @@
+library(shiny)
+library(leaflet)
+library(dplyr)
+library(DT)
+library(stringr)
+
+bballArenas <- read.csv("Data/CollegeBBallArenas.csv")
+
+server <- function(input, output) {
+  output$conference <- renderUI({
+    selectInput("conferenceInput",
+                "Select a Conference(s)",
+                choices = bballArenas$CONF,
+                selectize = TRUE,
+                multiple = TRUE)
+  })
+  
+  selectedConf <- reactive({
+    req(input$conferenceInput)
+    bballArenas %>% 
+      filter(CONF == input$conferenceInput)
+  })
+  
+  output$teamsDT <- DT::renderDataTable(DT::datatable({
+    selectedConf()
+    }))
+  
+  arena_data <- reactive({
+    team_arena_data <- selectedConf() %>% 
+      mutate(popup = str_c(str_c("School: ", Team, sep = ""),
+                           str_c("Arena: ", Arena, sep = ""),
+                           str_c("City: ", City, sep = ""),
+                           str_c("State: ", State, sep = ""),
+                           str_c("Capacity: ", Capacity, sep = ""),
+                           str_c("Conference: ", CONF, sep = ""),
+                           sep = "<br/>"))
+    team_arena_data
+  })
+  
+  observeEvent(input$showMap, {
+    output$Conf_Map <-renderLeaflet({
+        ncaa_map <- leaflet(arena_data()) %>% 
+          addProviderTiles("CartoDB.Positron")  %>% 
+          setView(-98.35, 39.7,
+                  zoom = 4) %>% 
+          addMarkers(~Longitude, ~Latitude,
+                     popup = ~popup)
+        ncaa_map  
+    }) 
+  })
+}
+
+ui <- fluidPage(
+  tags$b(tags$h1("Locations of College Basketball Arenas")),
+  sidebarLayout(
+    sidebarPanel(
+      uiOutput("conference"),
+      actionButton("showMap",
+                   "Show/Update Map",
+                   class = "btn-primary"),
+      radioButtons("displayTable",
+                   "Do you want to display a table?",
+                   c("Yes" = TRUE,
+                     "No" = FALSE),
+                   inline = TRUE,
+                   selected = FALSE)),
+    mainPanel(
+      leafletOutput("Conf_Map"),
+      br(),
+      conditionalPanel(condition = "input.displayTable == 'TRUE'",
+                       DT::dataTableOutput("teamsDT"))
+    )))
+
+shinyApp(ui, server)
