d24fe41ebbe54f7dd4a6d23eeb862a2e45bec5dc
Joe <jjenkin7@u.rochester.edu>
2020-11-03 16:23:39 -0500

    Basic Example

---
+++ ShinyBasicExample.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(dplyr)
+
+schedule <- read.csv("Data/NDSchedule.csv")
+
+ui <- fluidPage(
+  br(),
+  column(3,
+         selectInput("teamSelected",
+                     "Select an Opponent:",
+                     choices = schedule$Opponent)),
+  column(9,
+         textOutput("text"))
+)
+
+server <- function(input, output) {
+  
+  output$text <- renderText({
+    selectedGame <- schedule %>% 
+      filter(Opponent == input$teamSelected)
+    paste("Notre Dame will play ",
+          selectedGame$Opponent,
+          " on ",
+          as.character(selectedGame$Date),
+          " at ",
+          as.character(selectedGame$GameTime),
+          " in ",
+          as.character(selectedGame$Location),
+          ".",
+          sep = "")
+  })
+}
+
+shinyApp(ui, server)
