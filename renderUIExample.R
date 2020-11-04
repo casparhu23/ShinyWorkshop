b79ac8258c0ad7c6016f49547add71647ab2c6d0
Joe <jjenkin7@u.rochester.edu>
2020-11-03 19:48:20 -0500

    renderUI Example

---
+++ renderUIExample.R
@@ -0,0 +1,37 @@
+library(shiny)
+
+schedule <- read.csv("Data/NDSchedule.csv")
+
+server <- function(input, output) {
+  output$TeamSelectedUIOutput <- renderUI({
+    selectInput("teamSelected",
+                "Select an Opponent:",
+                choices = schedule$Opponent)
+  })
+  
+  output$text <- renderText({
+    req(input$teamSelected)
+    selectedGame <- schedule %>% 
+      filter(Opponent == input$teamSelected)
+    paste("Notre Dame will play ",
+          selectedGame$Opponent,
+          " on ",
+          as.character(selectedGame$Date),
+          " at ",
+          as.character(selectedGame$GameTime),
+          " in ",
+          as.character(selectedGame$Location),
+          ".",
+          sep = "")
+  })
+}
+
+ui <- fluidPage(
+  br(),
+  column(5,
+         uiOutput("TeamSelectedUIOutput")),
+  column(7,
+         textOutput("text"))
+)
+
+shinyApp(ui, server)
